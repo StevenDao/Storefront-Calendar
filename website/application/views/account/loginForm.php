3401deb2e41141429239fdc625a56c34bb10215a
fatalazn <fatalazn134@gmail.com>
2013-10-12 16:30:58 -0400

    reverting back to old login page for now

--- website/application/views/account/loginForm.php
+++ website/application/views/account/loginForm.php
@@ -2,59 +2,61 @@
 
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" media="all" href="<?= base_url() ?>css/reset.css"/>
-		<link rel="stylesheet" type="text/css" media="all" href="<?= base_url() ?>css/style.css"/>
+		<link rel="stylesheet" type="text/css" media="all" href="<?= base_url() ?>css/loginpage.css"/>
 	</head>
-<body>
+<body class="loginPage">
 
-<header class="topbg"></header>
+<div class="centered">
 
-<header class="main">
 <?php
 	if (isset($errorMsg)) {
 		echo "<p>" . $errorMsg . "</p>";
 	}
 
+
 	echo form_open('account/login');
 ?>
-
-<aside class="login">
-	<h5>
+<ul>
+	<li>
+		<h5>
 		<?php
 			echo anchor('account/newForm','Create Account') . '&nbsp &nbsp';
 			echo anchor('account/recoverPasswordForm','Recover Password');
 		?>
-	</h5>
+		</h5>
 
-	<?php
-
-	$attributes = array(
-		'name' => 'username',
-		'placeholder' => 'username'
-	);
-
-	echo '<h2>Login</h2>';
-	echo '<div>';
+		<h2>Login</h2>
+	</li>
+	<li>
+<?php
+	echo form_label('Username');
 	echo form_error('username');
-	echo form_input($attributes,set_value('username'),"required");
-	echo '</div>';
+	echo form_input('username',set_value('username'),"required");
+?>
+	</li>
 
-	echo '<div>';
+	<li>
+<?php
+	echo form_label('Password');
 	echo form_error('password');
 	echo form_password('password','',"required");
-	echo '</div>';
-
+?>
+	</li>
+<?php
 	$attributes = array(
 		'name' => 'submit',
 		'class' => 'submit');
-
+?>
+<?php
 	echo form_submit($attributes, 'Login');
+?>
+</ul>
+
+<?php
 	echo form_close();
 ?>
 
-</aside>
-
-</header>
+</div>
 
 </body>
 
