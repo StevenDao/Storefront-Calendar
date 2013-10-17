88ee93b0403c9e00874b8b5c7265d223eda8fcd3
Thurairaj Letchumanan <thurairaj92@gmail.com>
2013-10-16 20:44:59 -0400

    Update loginForm.php

--- website/application/views/account/loginForm.php
+++ website/application/views/account/loginForm.php
@@ -1,64 +1,127 @@
 <!DOCTYPE html>
 
 <html>
-	<head>
-		<link rel="stylesheet" type="text/css" media="all" href="<?= base_url() ?>css/loginpage.css"/>
-	</head>
-<body class="loginPage">
+    <head>
+        <link rel="stylesheet" type="text/css" media="all" href="<?= base_url() ?>css/loginpage.css"/>
+        
+    </head>
+    <body>
+        
+        <div class="header">
+            
+        </div>
+        
+        <div class='banner'>
+            <img class='spImage' src="../../images/banner.png" height="auto" width="100%">
+        </div>
+        
+        
+        <div class="centered">
 
-<div class="centered">
+            
+        <?php
+            if (isset($errorMsg)) {
+                echo "<p>" . $errorMsg . "</p>";
+            }
+            echo form_open('account/login');
+            ?>
+            <ul>
+                <li>
+                    <h5>
+                        <?php
+                        echo anchor('account/recoverPasswordForm', 'Forgot Password');
+                        ?>
+                    </h5>
+                    <h2>Login</h2>
+                </li>
+                <li>
+                    <?php
+                   
+                    echo form_error('username');
+                    echo form_input(array(
+                                    'name' => 'username',
+                                    'value' => set_value('username'),
+                                    'placeholder' => 'Username',
+                                    'required' => 'required'
+              
+                                    ));
+                    ?>
+                </li>
 
-<?php
-	if (isset($errorMsg)) {
-		echo "<p>" . $errorMsg . "</p>";
-	}
+                <li>
+                    <?php
+                    echo form_error('password');
+                    echo form_password(array(
+                                    'name' => 'password',
+                                    'value' => '',
+                                    'placeholder' => 'Password',
+                                    'required' => 'required'
+              
+                                    ));
+                    ?>
+                </li>
+                <?php
+                $attributes = array(
+                    'name' => 'submit',
+                    'class' => 'submit');
+                ?>
+                <?php
+                echo form_submit($attributes, 'Login');
+                ?>
+            </ul>
+            <?php
+            echo form_close();
+            ?>
+        </div>
+        
+        <footer>
+            
+            <div>
+                <section>
+                    <h6>About</h6>
+                    <ul class="special">
+                        <li>
+                            The East Storefront provides services for the community in the East Scarborough region <br>
+                            bla bla bla bla bla bla someone good with english please edit this area, I'm not the  <br>
+                            for this task, please don't kill me!! :D 
+                        </li> 
+                    </ul>
+                    </section>
+                    <section>
+                    <h6>Address</h6>
+                    <ul class='special'>
+                        <li>
+                            1 Herderson Drive
+                        </li>
+                    </ul>
+                    </section>
+                
+                    <section>
+                    <h6>Links</h6>
+                    <ul class='special'>
+                        <li>
+                            <a href='#'> Nope, the link takes nowhere</a>
+                        </li>
+                        <li>
+                            <a href='#'> Nope, the links take nowhere</a>
+                        </li>
+                        <li>
+                            <a href='#'> Nope, the links take nowhere</a>
+                        </li>
+                    </ul>
+                    </section>
+                    
+                
+            </div>
+            
+            
+        </footer>
+        
+            
 
 
-	echo form_open('account/login');
-?>
-<ul>
-	<li>
-		<h5>
-		<?php
-			echo anchor('account/newForm','Create Account') . '&nbsp &nbsp';
-			echo anchor('account/recoverPasswordForm','Recover Password');
-		?>
-		</h5>
 
-		<h2>Login</h2>
-	</li>
-	<li>
-<?php
-	echo form_label('Username');
-	echo form_error('username');
-	echo form_input('username',set_value('username'),"required");
-?>
-	</li>
-
-	<li>
-<?php
-	echo form_label('Password');
-	echo form_error('password');
-	echo form_password('password','',"required");
-?>
-	</li>
-<?php
-	$attributes = array(
-		'name' => 'submit',
-		'class' => 'submit');
-?>
-<?php
-	echo form_submit($attributes, 'Login');
-?>
-</ul>
-
-<?php
-	echo form_close();
-?>
-
-</div>
-
-</body>
+    </body>
 
 </html>
 
