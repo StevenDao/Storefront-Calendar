d60eb52656b3167ee7cfb54d88262f9f80385cc7
yevtechnic <zhenya.khvan@gmail.com>
2013-10-17 18:35:35 -0400

    readded submit button

--- website/application/views/account/loginForm.php
+++ website/application/views/account/loginForm.php
@@ -60,6 +60,15 @@
                                     ));
                     ?>
                 </li>
+                <?php
+                $attributes = array(
+                    'name' => 'submit',
+                    'style' => 'position: absolute; left: -9999px; width: 1px; height: 1px;'
+                );
+                ?>
+                <?php
+                echo form_submit($attributes, 'Login');
+                ?>
                 
             </ul>
             <?php
