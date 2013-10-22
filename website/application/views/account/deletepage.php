801f611a34142e0f33fef5eef3aa3a4ad758e516
clark <huajie.chen@utoronto.ca>
2013-10-22 04:23:25 -0400

    roll back

--- website/application/views/account/deletepage.php
+++ website/application/views/account/deletepage.php
@@ -5,12 +5,14 @@
 		<h1>please choose the user you want to delete</h1>
 	</head>
 	<body>		
-		<?php foreach($query as $row): ?>
-		<tr>
-			<td><?php echo $row->login; ?></td>
-			<td><?php echo $row->first; ?></td>
-			<td><?php echo $row->last; ?></td>
-		</tr>
-		<?php endforeach; ?>
+		<tbody>
+			<?php foreach($query as $row): ?>
+			<tr>
+				<td><?php echo $row->login; ?></td>
+				<td><?php echo $row->first; ?></td>
+				<td><?php echo $row->last; ?></td>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
 	</body>
 </html>
