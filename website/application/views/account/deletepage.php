6ab0233855f67b8b410e692b83c3a4f4e9c792c4
clark <huajie.chen@utoronto.ca>
2013-10-22 04:22:08 -0400

    making table

--- website/application/views/account/deletepage.php
+++ website/application/views/account/deletepage.php
@@ -2,16 +2,15 @@
 
 <html>
 	<head>
+		<h1>please choose the user you want to delete</h1>
 	</head>
-	<body>
-		<tbody>
-			<?php foreach($query as $row): ?>
-			<tr>
-				<td><?php echo $row->login; ?></td>
-				<td><?php echo $row->first; ?></td>
-				<td><?php echo $row->last; ?></td>
-			</tr>
-			<?php endforeach; ?>
-		</tbody>
+	<body>		
+		<?php foreach($query as $row): ?>
+		<tr>
+			<td><?php echo $row->login; ?></td>
+			<td><?php echo $row->first; ?></td>
+			<td><?php echo $row->last; ?></td>
+		</tr>
+		<?php endforeach; ?>
 	</body>
 </html>
