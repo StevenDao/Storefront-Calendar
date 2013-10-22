10ed0c4d873cc94e53254067a286b4b07f95fd0d
clark <huajie.chen@utoronto.ca>
2013-10-22 04:51:50 -0400

    tryout

--- website/application/views/account/deletepage.php
+++ website/application/views/account/deletepage.php
@@ -13,7 +13,7 @@
 						<td><?php echo $row->login; ?></td>
 						<td><?php echo $row->first; ?></td>
 						<td><?php echo $row->last; ?></td>
-						<td><INPUT TYPE="Submit" Name ="login" value = <?php echo $row->login; ?></td>
+						<td><INPUT TYPE="Submit" Name = <form action="account/delete_user" method="post">value="Delete"></td>
 					</tr>
 				<?php endforeach; ?>
 			</tbody>
