e6c775c20c7c3cd7f70fece235da99d9db0d399d
clark <huajie.chen@utoronto.ca>
2013-10-22 01:00:18 -0400

    typo

--- website/application/views/main.php
+++ website/application/views/main.php
@@ -14,8 +14,8 @@ if (isset($message))
 
 <ul>
 	<li><?= anchor("account/create_new_user", 'Create New User'); ?></li>
-	<li><?= anchor("account/delete_user", 'Delete User')?></li>
-	<li><?= anchor("account/modify_user", 'Modify User')?></li>
+	<li><?= anchor("account/delete_user", 'Delete User'); ?></li>
+	<li><?= anchor("account/modify_user", 'Modify User'); ?></li>
  	<li><?= anchor("account/logout", 'Logout'); ?></li>
 </ul>
 
