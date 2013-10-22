7be79476ceba4869915b3725aecc094e49ebd440
clark <huajie.chen@utoronto.ca>
2013-10-22 04:26:16 -0400

    ..

--- website/application/views/account/deletepage.php
+++ website/application/views/account/deletepage.php
@@ -4,15 +4,17 @@
 	<head>
 		<h1>please choose the user you want to delete</h1>
 	</head>
-	<body>		
-		<tbody>
-			<?php foreach($query as $row): ?>
-			<tr>
-				<td><?php echo $row->login; ?></td>
-				<td><?php echo $row->first; ?></td>
-				<td><?php echo $row->last; ?></td>
-			</tr>
-			<?php endforeach; ?>
-		</tbody>
+	<body>
+		<table border = "1">		
+			<tbody>
+				<?php foreach($query as $row): ?>
+					<tr>
+						<td><?php echo $row->login; ?></td>
+						<td><?php echo $row->first; ?></td>
+						<td><?php echo $row->last; ?></td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
 	</body>
 </html>
