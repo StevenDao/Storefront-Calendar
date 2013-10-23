7b58a23fd46a2015524ac07b420dba7ac4f19d4c
jmccool <jp.mccool@mail.utoronto.ca>
2013-10-23 14:16:24 -0400

    Added room.php and room_model.php, commented user_model.php

--- website/application/models/user_model.php
+++ website/application/models/user_model.php
@@ -1,60 +1,68 @@
 <?php
-class User_model extends CI_Model
-{
-	function get($username) {
-		$this->db->where('login',$username);
-		$query = $this->db->get('user');
-		if ($query && $query->num_rows() > 0)
-			return $query->row(0,'User');
-		else
-			return null;
-	}
-
-	function getFromId($id) {
-		$this->db->where('id',$id);
-		$query = $this->db->get('user');
-		if ($query && $query->num_rows() > 0)
-			return $query->row(0,'User');
-		else
-			return null;
-	}
-
-	function getFromEmail($email) {
-		$this->db->where('email',$email);
-		$query = $this->db->get('user');
-		if ($query && $query->num_rows() > 0)
-			return $query->row(0,'User');
-		else
-			return null;
-	}
-
-	function insert($user) {
-		return $this->db->insert('user',$user);
-	}
-
-	function updatePassword($user) {
-		$this->db->where('id',$user->id);
-		return $this->db->update('user',array('password'=>$user->password,
-				                                'salt' => $user->salt));
-	}
-
-	function getExclusive($username) {
-		$sql = "select * from user where login=? for update";
-		$query = $this->db->query($sql,array($username));
-		if ($query && $query->num_rows() > 0)
-			return $query->row(0,'User');
-		else
-			return null;
-	}
-	
-	function displayAllUsers() {
-		$query = $this->db->select('*')->from('user')->get();
-		return $query->result();
-	}
-	
-	function deleteUser($username){
-		$this->db->where('login', $username);
-		$this->db->delete('user');
-	}
+class User_model extends CI_Model {
+    
+    // Get a user's record from the 'user' table using their login as a key
+    function get($username) {
+        $this->db->where('login',$username);
+        $query = $this->db->get('user');
+        if ($query && $query->num_rows() > 0)
+            return $query->row(0,'User');
+        else
+            return null;
+    }
+    
+    // Get a user's record from the 'user' table using their id as a key
+    function getFromId($id) {
+        $this->db->where('id',$id);
+        $query = $this->db->get('user');
+        if ($query && $query->num_rows() > 0)
+            return $query->row(0,'User');
+        else
+            return null;
+    }
+    
+    // Get a user's record from the 'user' table using their email as a key
+    function getFromEmail($email) {
+        $this->db->where('email',$email);
+        $query = $this->db->get('user');
+        if ($query && $query->num_rows() > 0)
+            return $query->row(0,'User');
+        else
+            return null;
+    }
+    
+    // Insert a new user into the 'user' table
+    function insert($user) {
+        return $this->db->insert('user',$user);
+    }
+    
+    // Update the password of an existing user
+    function updatePassword($user) {
+        $this->db->where('id',$user->id);
+        return $this->db->update('user',array('password'=>$user->password,
+                                                'salt' => $user->salt));
+    }
+    
+    // Exclusive lookup of a user
+    function getExclusive($username) {
+        $sql = "select * from user where login=? for update";
+        $query = $this->db->query($sql,array($username));
+        if ($query && $query->num_rows() > 0)
+            return $query->row(0,'User');
+        else
+            return null;
+    }
+    
+    // Show users
+    function displayAllUsers() {
+        $query = $this->db->select('*')->from('user')->get();
+        return $query->result();
+    }
+    
+    // Delete a user based on username
+    function deleteUser($username){
+        $this->db->where('login', $username);
+        $this->db->delete('user');
+    }
 }
 ?>
