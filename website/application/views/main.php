17ab365c5a6dc98cb134e3edb3ff8ea51403717d
yevtechnic <zhenya.khvan@gmail.com>
2013-10-27 16:59:13 -0400

    editing layout and retreiving booking from db works

--- website/application/views/main.php
+++ website/application/views/main.php
@@ -1,95 +1,41 @@
 <html>
-    <head>
-        <title></title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-        <link rel="stylesheet" type='text/css' href="<?= base_url() ?>css/ui.slider.extras.css" />
-        <link rel='stylesheet' type='text/css' href="<?= base_url() ?>js/fullcalendar-1.6.4/fullcalendar/fullcalendar.css" />
+<link rel="stylesheet" type="text/css" href="<?= base_url() ?>css/reset.css" />
+<link rel="stylesheet" href="<?= base_url() ?>fullcalendar/fullcalendar.css" />
 
-        <script src="<?= base_url() ?>js/fullcalendar-1.6.4/lib/jquery.min.js"></script>
-        <script src="<?= base_url() ?>js/fullcalendar-1.6.4/lib/jquery-ui.custom.min.js"></script>
-        <script src="<?= base_url() ?>js/fullcalendar-1.6.4/fullcalendar/fullcalendar.min.js"></script>
+<script src='<?= base_url() ?>fullcalendar/lib/jquery.min.js'></script>
+<script src='<?= base_url() ?>fullcalendar/lib/jquery-ui.custom.min.js'></script>
+<script src="<?= base_url() ?>/js/jquery.timers.js"></script>
+<script src='<?= base_url() ?>fullcalendar/fullcalendar.min.js'></script>
 
-        <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+<link rel="stylesheet" href="<?= base_url() ?>css/clientPage.css" />
+<script src='<?= base_url() ?>js/calendarView.js'></script>
 
-        <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
-        <script type="text/javascript" src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-        <script type="text/javascript" src="<?= base_url() ?>js/calendarView.js"></script>
-        <script type="text/javascript" src="<?= base_url() ?>/js/selectToUISlider.jQuery.js"></script>
-        <script type='text/javascript' src="<?= base_url() ?>js/fullcalendar-1.6.4/fullcalendar/fullcalendar.js"></script>
+</head>
 
-        <link rel="stylesheet" href="<?= base_url() ?>css/clientPage.css" />
-    </head>
-    <body class="body">
-        <div>
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <a href="#" class="logo-link">
+            Storefront Calendar<span id="logo-caret" class="icon"></span>
+          </a>
+          <ul>
+            <li><a href="#">Add New User</a></li>
+            <li><a href="#">Edit User</a></li>
+            <li><a href="#">Add New Client</a></li>
+            <li><a href="#">Edit Client</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </header>
 
-            <nav>
-                <ul>
-                    <li>
-                        <button>
-                            <input class="imageIcon" type="image" src="<?= base_url() ?>images/b_actions.png"/>
+  <div id="calendar" style="z-index: -99999;" ></div>
 
-                            <ul>
-                                <li><?= anchor('account/form_new_user', 'Create New User') ?></li>
-                                <li><?= anchor('account/modifyuser', 'Modify User') ?></li>
-                                <li><?= anchor('account/form_new_client', 'Create New Client') ?></li>
-                                <li><?= anchor('account/form_edit_client', 'Edit Client') ?></li>
-                                <li><a href="#">Do..</a></li>
-                            </ul>
-
-                        </button>
-                    </li>
-
-                    <li class="special">
-                        <h5 class="header">STOREFRONT CALENDER</h5>
-                    </li>
-
-                    <li style="float:right; margin-right:20px;">
-                        <button>
-                            <input class="imageIcon" type="image" src="<?= base_url() ?>images/b_actions.png"/>
-                            <ul style="left:85%;">
-                                <li><a href="#">Email</a></li>
-                                <li><a href="#">User Experience</a></li>
-                            </ul>
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-
-
-            <div id="views" class="views">
-
-                <button id="days" class="button">Days</button>
-                <button id="agendaWeek" class="button">Week</button>
-                <button id="month" class="button">Month</button>
-                <button id="rooms" class="button">Room</button>
-
-            </div>
-
-            <div class="calender">
-
-                <button id="prev" style="z-index: 3;"></button>
-                <button id="next"></button>
-                <div id="calendar" ></div>
-
-            </div>
-
-
-            <div class="bottom">
-
-                <select name="speed" id="monthsSli" style="display:none">
-                    <option value="Slower">Slower</option>
-                    <option value="Slow">Slow</option>
-                    <option value="Med">Med</option>
-                    <option value="Fast">Fast</option>
-                    <option value="Faster">Faster</option>
-                    </select>
-
-            </div>
-
-        </div>
-
-    </body>
+</body>
 </html>
-
-
