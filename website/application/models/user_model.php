141c92c9028463eb9c1076b77d97169366fdabfb
Thurairaj Letchumanan <thurairaj92@gmail.com>
2013-11-05 07:32:41 -0500

    DONE FIXING!!

--- website/application/models/user_model.php
+++ website/application/models/user_model.php
@@ -1,76 +1,129 @@
 <?php
 
-class Client_model extends CI_Model
-{
-        function get_from_name($name) {
-                $this->db->where('name', $name);
-                $query = $this->db->get('client');
-                if ($query && $query->num_rows() > 0)
-                        return $query->row(0, 'Client');
-                else
-                        return null;
-        }
+class User_model extends CI_Model {
+    /*
+     * Get a user's record from the 'user' table using their login as a key.
+     */
+    function get($username) {
+        $this->db->where('login', $username);
+        $query = $this->db->get('user');
+        if ($query && $query->num_rows() > 0)
+            return $query->row(0, 'User');
+        else
+            return null;
+    }
 
-        function get_from_id($id) {
-                $this->db->where('id', $id);
-                $query = $this->db->get('client');
-                if ($query && $query->num_rows() > 0)
-                        return $query->row(0, 'Client');
-                else
-                        return null;
-        }
+    /*
+     * Get a user's record from the 'user' table using their id as a key.
+     */
+    function get_from_id($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->get('user');
 
-        function get_from_email($email) {
-                $this->db->where('email',$email);
-                $query = $this->db->get('client');
-                if ($query && $query->num_rows() > 0)
-                        return $query->row(0,'Client');
-                else
-                        return null;
-        }
+        if ($query && $query->num_rows() > 0)
+            return $query->row(0, 'User');
+        else
+            return null;
+    }
 
-        function get_clients() {
-                $clients = array();
-                $query = $this->db->query("SELECT * FROM client;");
+    /*
+     * Get a user's record from the 'user' table using their email as a key.
+     */
+    function get_from_email($email) {
+        $this->db->where('email', $email);
+        $query = $this->db->get('user');
 
-                foreach ($query->result('Client') as $row) {
-                        $clients[$row->id] = $row->name;
-                }
+        if ($query && $query->num_rows() > 0)
+            return $query->row(0, 'User');
+        else
+            return null;
+    }
 
-                return $clients;
-        }
+    /*
+     * Insert a new user into the 'user' table.
+     */
+    function insert($user) {
+        return $this->db->insert('user', $user);
+    }
 
-        function insert($client) {
-                return $this->db->insert('client', $client);
-        }
+    /*
+     * Update the email address of an existing user.
+     */
+    function update_email($user) {
+        $this->db->where('id', $user->id);
 
-        function update_address($client) {
-                $this->db->where('id', $client->id);
-                return $this->db->update('client', array('address' => $client->address));
-        }
+        return $this->db->update('user', array('email' => $user->email));
+    }
 
-        function get_exclusive($name) {
-                $sql = "SELECT * FROM client WHERE login=? FOR UPDATE";
-                $query = $this->db->query($sql, array($name));
-                if ($query && $query->num_rows() > 0)
-                        return $query->row(0, 'Client');
-                else
-                        return null;
-        }
-        
-    // Show all clients
-    function display_all_clients() {
-        $query = $this->db->select('*')->from('client')->get();
+    /*
+     * Update the first and last names of an existing user.
+     */
+    function update_name($user) {
+        $this->db->where('id', $user->id);
+
+        return $this->db->update(
+                        'user', array('first' => $user->first, 'last' => $user->last)
+        );
+    }
+
+    /*
+     * Update permission level of user
+     */
+    function update_usertype($user) {
+        $this->db->where('id', $user->id);
+        return $this->db->update('user', array('usertype' => $user->usertype));
+    }
+
+    /*
+     * Update clientid of user
+     */
+    function update_clientid($user) {
+        $this->db->where('id', $user->id);
+        return $this->db->update('user', array('clientid' => $user->clientid));
+    }
+
+
+    /*
+     * Update the password of an existing user.
+     */
+    function update_password($user) {
+        $this->db->where('id', $user->id);
+
+        return $this->db->update(
+                        'user', array('password' => $user->password, 'salt' => $user->salt)
+        );
+    }
+
+    /*
+     * Exclusive lookup of a user.
+     */
+    function get_exclusive($username) {
+        $sql = "SELECT * FROM user WHERE login=? FOR UPDATE";
+        $query = $this->db->query($sql, array($username));
+
+        if ($query && $query->num_rows() > 0)
+            return $query->row(0, 'User');
+        else
+            return null;
+    }
+
+    /*
+     * Display all the users.
+     */
+    function display_all_users() {
+        $query = $this->db->select('*')->from('user')->get();
         return $query->result();
     }
-    
-    //TO DO: the users are deleting properly; apperantly I can't delete two times
-    function delete_client($id){
-    	$this->db->delete('user', array('clientid' => $id));
-        $this->db->delete('client', array('id' => $id));
-        
+
+    /*
+     * Delete a user based on username.
+     */
+    function delete_user($login) {
+        $this->db->where('login', $login);
+        $this->db->delete('user');
     }
+
 }
 
-/* End of file client_model.php */
-/* Location: ./application/models/client_model.php */
+/* End of file user_model.php */
+/* Location: ./application/models/user_model.php */
