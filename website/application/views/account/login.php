df4b8eb62bc5e00a48b3b03997d4bf95edda5a79
yevtechnic <zhenya.khvan@gmail.com>
2013-11-17 14:48:25 -0500

    updating login page

--- website/application/views/account/login.php
+++ website/application/views/account/login.php
@@ -1,132 +1,99 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <link rel="stylesheet" type="text/css" media="all" href="<?= base_url() ?>css/loginpage.css"/>
-        
-    </head>
-    <body>
-        
-        <div class="header"></div>
-        
-        <div class='banner'>
-            <img class='spImage' src="<?= base_url() ?>images/banner.png" height="auto" width="100%">
-        </div>
-        
-        
-        <div class="centered">
+<head>
+  <link rel="stylesheet" type="text/css" media="all" href="<?= base_url() ?>css/reset.css"/>
+  <link rel="stylesheet" type="text/css" media="all" href="<?= base_url() ?>css/loginpage.css"/>
+</head>
+<body>
+  <div id="container">
 
-            
-        <?php
-            if (isset($errorMsg)) {
-                echo "<p>" . $errorMsg . "</p>";
-            }
-            echo form_open('account/login');
-            ?>
-            <ul>
-                <li style="height:20px">
-                    <h5 class="specialH">Login</h5>
-                    <h5>
-                        <?php
-                        echo anchor('account/recover_password', 'Forgot Password');
-                        ?>
-                    </h5>
-                   
-                </li>
-                <li class="line"></li>
-                <li>
-                    <?php
-                   
-                    echo form_error('username');
-                    echo form_input(array(
-                                    'name' => 'username',
-                                    'value' => set_value('username'),
-                                    'placeholder' => 'Username',
-                                    'required' => 'required'
-              
-                                    ));
-                    ?>
-                </li>
+    <div class='banner'>
+      <img class='spImage' src="<?= base_url() ?>images/logo.png" height="auto" width="100%">
+    </div>
 
-                <li>
-                    <?php
-                    echo form_error('password');
-                    echo form_password(array(
-                                    'name' => 'password',
-                                    'value' => '',
-                                    'placeholder' => 'Password',
-                                    'required' => 'required'
-              
-                                    ));
-                    ?>
-                </li>
-                
-                  <?php
-                $attributes = array(
-                    'name' => 'submit',
-                    'style' => 'position: absolute; left: -9999px; width: 1px; height: 1px;'
-                );
-                ?>
-                <?php
-                echo form_submit($attributes, 'Login');
-                ?>
-                
-            </ul>
+    <div class="centered">
+      <?php
+      if (isset($errorMsg)) {
+          echo "<p>" . $errorMsg . "</p>";
+      }
+      echo form_open('account/login');
+      ?>
+      <ul>
+        <li style="height:20px">
+          <h5 class="specialH">Login</h5>
+          <h5>
             <?php
-            echo form_close();
+            echo anchor('account/recover_password', 'Forgot Password');
             ?>
-        </div>
-        
-        <img class="background" src="<?= base_url() ?>images/photo-Cover.jpg">
-        
-        <footer>
-            
-            <div>
-                <section>
-                    <h6>About</h6>
-                    <ul class="special">
-                        <li>
-                            The East Storefront provides services for the community in the East Scarborough region <br>
-                            bla bla bla bla bla bla someone good with english please edit this area, I'm not the  <br>
-                            for this task, please don't kill me!! :D 
-                        </li> 
-                    </ul>
-                    </section>
-                    <section>
-                    <h6>Address</h6>
-                    <ul class='special'>
-                        <li>
-                            1 Herderson Drive
-                        </li>
-                    </ul>
-                    </section>
-                
-                    <section>
-                    <h6>Links</h6>
-                    <ul class='special'>
-                        <li>
-                            <a href='#'> Nope, the link takes nowhere</a>
-                        </li>
-                        <li>
-                            <a href='#'> Nope, the links take nowhere</a>
-                        </li>
-                        <li>
-                            <a href='#'> Nope, the links take nowhere</a>
-                        </li>
-                    </ul>
-                    </section>
-                    
-                
-            </div>
-            
-            
-        </footer>
-        
-            
+          </h5>
 
+        </li>
+        <div class="line"></div>
+        <li>
+          <?php
 
+          echo form_error('username');
+          echo form_input(array(
+          'name' => 'username',
+          'value' => set_value('username'),
+          'placeholder' => 'Username',
+          'required' => 'required'
 
-    </body>
+          ));
+          ?>
+        </li>
 
+        <li>
+          <?php
+          echo form_error('password');
+          echo form_password(array(
+          'name' => 'password',
+          'value' => '',
+          'placeholder' => 'Password',
+          'required' => 'required'
+
+          ));
+          ?>
+        </li>
+
+        <?php
+        $attributes = array(
+        'name' => 'submit',
+        'style' => 'position: absolute; left: -9999px; width: 1px; height: 1px;'
+        );
+        ?>
+        <?php
+        echo form_submit($attributes, 'Login');
+        ?>
+
+      </ul>
+      <?php
+      echo form_close();
+      ?>
+    </div>
+    <div class="background">
+      <img src="<?= base_url() ?>images/photo-Cover.jpg">
+    </div>
+    <div class="footer">
+      <section id="about">
+        <h6>About</h6>
+        <p>
+          Scarborough is a safe, well-educated and prosperous community.  The
+          Storefront contributes to making the impossible possible by providing
+          accessible sites for community members of all ages and cultures to find
+          and share solutions they need to live healthy lives, find meaningful
+          work, play and thrive. We are seen as an excellent model for
+          sustainable social innovation and transformation in communities.
+        </p>
+      </section>
+      <section id="address">
+        <h6>Address</h6>
+        4040 Lawrence Ave E, Scarborough, ON M1E 2R6 <br />
+        (416) 208-9889
+      </section>
+      <div class=clear></div>
+    </div>
+  </div>
+</body>
 </html>
-
