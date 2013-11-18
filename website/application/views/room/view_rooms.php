e538fe73d7b6d6b93bdee76e5538063cd1b44ac9
Thurairaj Letchumanan <thurairaj92@gmail.com>
2013-11-18 17:06:04 -0500

    Update view_rooms.php

--- website/application/views/room/view_rooms.php
+++ website/application/views/room/view_rooms.php
@@ -1,11 +1,10 @@
-
 <img id="imgName" src="<?php echo base_url(); ?>images/mapSMALL.jpg" width="500" height="528" usemap="#usemap" style="margin:50px;margin-left:200px;">
 
 	<map name="usemap">
-		<area shape="rect" coords="35,5,140,180" alt="5">
+		<area shape="rect" coords="35,5,140,180" alt="4">
 		<area shape="rect" coords="0,230,43,288" alt="3">
 		<area shape="rect" coords="189,0,262,95" alt="7">
-		<area shape="rect" coords="308,449,380,527" alt="8">
+		<area shape="rect" coords="308,449,380,527" alt="5">
 		<area shape="rect" coords="405,300,486,414" alt="6">
 		<!--<area shape="rect" coords="380,411,486,527" alt="7">-->
 		<area shape="rect" coords="381,230,430,277" alt="1">			
